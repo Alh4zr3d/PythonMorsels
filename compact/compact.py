a36640b032e144c8afe92158fff3a59e8f817683
Ruri <RuriRurouni@gmail.com>
2019-10-05 16:18:19 -0600

    compact.py

---
+++ compact/compact.py
@@ -0,0 +1,20 @@
+from itertools import groupby
+
+def compact(data):
+    cmpcted = [i for i,_ in groupby(data)]
+    return iter(cmpcted)
+
+# data = [1, 1, 2, 2, 3, 2]
+# groups = []
+# uniquekeys = []
+# data = sorted(data)
+# for k, g in groupby(data):
+#    groups.append(list(g))      # Store group iterator as a list
+#    uniquekeys.append(k)
+
+# print(groups)
+# print(uniquekeys)
+
+# cmpcted = [i for i,_ in groupby(data)]
+# print(cmpcted)
+# print(iter(cmpcted))
