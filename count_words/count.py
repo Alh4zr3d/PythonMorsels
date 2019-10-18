89520e4d12d9916fb6374b1c8c0778d4e39201fb
Joe Kuensting <RuriRurouni@gmail.com>
2019-10-18 14:02:43 -0600

    Finished count_words

--- count_words/count.py
+++ count_words/count.py
@@ -1,10 +1,14 @@
-import re
-
 def count_words(str):
     result = {}
-    words = re.findall(r'\w+',str)
+    wordchars = "abcdefghijklmnopqrstuvwxyz'"
+    words = str.split()
     for word in words:
-        if word.lower() not in result.keys():
+        word = word.lower()
+        if word[0] not in wordchars:
+            word = word[1:]
+        if word[-1] not in wordchars:
+            word = word[0:-1]
+        if word not in result.keys():
             result[word] = 1
         else:
             result[word] += 1
